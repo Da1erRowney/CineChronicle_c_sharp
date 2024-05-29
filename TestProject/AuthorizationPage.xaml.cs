ae84d81333e021d0f597d87bf9772b1d65bd65f8
Konstantin Chernenko <konstantin032005@gmail.com>
2024-05-29 22:54:03 +0300

    Кодик для авторизации

--- TestProject/AuthorizationPage.xaml.cs
+++ TestProject/AuthorizationPage.xaml.cs
@@ -1,3 +1,5 @@
+using DataContent;
+
 namespace TestProject;
 
 public partial class AuthorizationPage : ContentPage
@@ -9,11 +11,30 @@ public partial class AuthorizationPage : ContentPage
 
     private void OnAddClicked(object sender, EventArgs e)
     {
-		//....
+        //....
 
-		var user = new User
-		{
+        DatabaseServiceContent databaseService = new DatabaseServiceContent(MainPage._databasePath);
 
-		}
+        if (databaseService.GetAuthorizedByAuth(true) != null)
+        {
+            var authUser = databaseService.GetAuthorizedByAuth(true);
+            authUser.IsAuthenticated = false;
+            databaseService.UpdateAuth(authUser);
+        }
+
+        var user = new User
+        {
+            Email = EmailEntry.Text,
+            Password = PasswordEntry.Text
+        };
+        databaseService.InsertUser(user);
+
+        var authenticated = new Authorized
+        {
+            Email = user.Email,
+            IsAuthenticated = true
+        };
+        databaseService.InsertAuth(authenticated);
+
     }
 }

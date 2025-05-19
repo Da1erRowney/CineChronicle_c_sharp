902a8a8e177c12f825f3e322c48fd7b61bed4e0c
Konstantin <126601293+Da1erRowney@users.noreply.github.com>
2025-05-19 10:37:27 +0300

    Update User.cs

--- TestProject/Tables/User.cs
+++ TestProject/Tables/User.cs
@@ -4,10 +4,11 @@ namespace CineChronicle.Tables
 {
     public class User
     {
-        [PrimaryKey]
+        [PrimaryKey,AutoIncrement]
+        public int Id {get; set; }
         public string Email { get; set; }
+        public string NickName {get; set;}
         public string Password { get; set; }
-
         public string NameIcon { get; set; }
     }
 }
